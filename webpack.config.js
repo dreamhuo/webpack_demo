71ec047fd2f94b13f3a9cb21596e4b3aadc07f8c
TOSPUR\63081 <huochangsheng@tospur.com>
2019-02-14 13:27:47 +0800

    添加配置

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,9 @@
-webpack.config.js
+var path = require("path");
+
+module.exports = {
+    entry: ['./src/common.js', './src/page.js'],
+    output: {
+        path: path.join(__dirname, './public/js'),
+        filename: 'base.js'
+    }
+};
