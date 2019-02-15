569b93dcd5d3f51b61b9b3dcf9b3c92da60a748a
TOSPUR\63081 <huochangsheng@tospur.com>
2019-02-15 09:13:41 +0800

    合并文件

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,23 @@
 var path = require("path");
-
-module.exports = {
-    entry: ['./src/common.js', './src/page.js'],
+var config = {
+    mode: 'production',
+    entry: {
+        common: ['./js/common.js', './js/page.js'],
+        page: './js/page.js'
+    },
     output: {
+        // path: path.join(__dirname, './public/js'),
+        // filename: 'base.js'
+        filename: '[name].js',
         path: path.join(__dirname, './public/js'),
-        filename: 'base.js'
+        publicPath: '/dist/'
     }
+};
+
+module.exports = (env, argv) => {
+    if (argv.mode === 'development') {
+    }
+    if (argv.mode === 'production') {
+    }
+    return config;
 };
